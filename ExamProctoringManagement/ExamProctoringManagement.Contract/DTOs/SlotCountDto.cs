8fe62029b6799d969fd2d31966eabb9fcc41f5b7
MaxNekori <thp3963043@gmail.com>
2024-11-08 00:31:01 +0700

    add GetCurrentSlotCount

---
+++ ExamProctoringManagement/ExamProctoringManagement.Contract/DTOs/SlotCountDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProctoringManagement.Contract.DTOs
+{
+    public class SlotCountDto
+    {
+        public int SlotCount { get; set; }
+        public float TotalTime { get; set; }
+    }
+}
